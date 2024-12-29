8e598f33c7e28bb80bf29ea951c6c799e8437bc3
Mikael Weiss <campingmikael@icloud.com>
2024-12-29 13:50:19 -0700

    Updated the mail stuff so that mail doesn't fall through

--- lib/strive_planner/email.ex
+++ lib/strive_planner/email.ex
@@ -2,7 +2,7 @@ defmodule StrivePlanner.Email do
   def contact_form_email(name, email, subject, message) do
     Resend.Emails.send(%{
       from: "Strive Planner <support@striveplanner.org>",
-      to: [email],
+      to: "campingmikael@icloud.com",
       subject: "Contact Form Submission: #{subject}",
       html: """
       <h2>New Contact Form Submission</h2>
