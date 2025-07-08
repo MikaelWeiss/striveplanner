fce67343972accfff99dc9e6a9e04b6673c499ca
Mikael Weiss <campingmikael@icloud.com>
2025-07-08 14:16:54 -0600

    added dotenv for development

--- mix.exs
+++ mix.exs
@@ -55,7 +55,8 @@ defmodule StrivePlanner.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:resend, "~> 0.4.0"}
+      {:resend, "~> 0.4.0"},
+      {:dotenv, "~> 3.0.0", only: [:dev, :test]}
     ]
   end
 
