e7e5688fe6a3685301784c45de6053fa251c552d
Mikael Weiss <campingmikael@icloud.com>
2025-11-24 15:02:25 -0700

    Got it running!

--- config/runtime.exs
+++ config/runtime.exs
@@ -52,6 +52,7 @@ if config_env() == :prod do
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     # For machines with several cores, consider starting multiple pools of `pool_size`
     # pool_count: 4,
+    socket_dir: "/var/run/postgresql",
     socket_options: maybe_ipv6
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
